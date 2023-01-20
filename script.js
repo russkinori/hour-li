a9ce4737aaa7939867727fe7dd9da7da43000e51
Kinori <russel.richard@outlook.com>
2023-01-20 20:08:46 +0000

    added function for creating timeblocks in js file

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //Display the current day at the top of the calender when a user opens the planner.
-$("#currentDay").text(moment().format("dddd MMMM do"));
+$("#currentDay").text(moment().format("dddd, MMMM do"));
+
 //Present timeblocks for standard business hours when the user scrolls down.
+    //Create rows with 3 columns of varying sizes
+    //Create a event listener for scrolling
 $("#calendar-grid").append(
-    $("#time").val(moment().format("dddd MMMM do"))
+    
 )
+
 //Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+    //link the event column with a time and color
+        //Link past hours with grey, present with red and future with green 
 
 //Allow a user to enter an event when they click a timeblock
 
